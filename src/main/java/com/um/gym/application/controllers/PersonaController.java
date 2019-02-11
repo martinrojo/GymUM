77d24001419c9477a0d6cf2db48f6155db150e48
Martín Rojo <martinrojo76@gmail.com>
2019-02-11 12:31:39 -0300

    Borrado los utils. Cambiado los return de los controladores.

--- src/main/java/com/um/gym/application/controllers/PersonaController.java
+++ src/main/java/com/um/gym/application/controllers/PersonaController.java
@@ -2,7 +2,6 @@ package com.um.gym.application.controllers;
 
 import com.um.gym.application.models.Persona;
 import com.um.gym.application.service.impl.PersonaServiceImpl;
-import com.um.gym.application.utils.JsonUsuarioController;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,26 +19,7 @@ public class PersonaController {
     @Autowired
     private PersonaServiceImpl personaService;
 
-    @Autowired
-    private JsonUsuarioController jsonUsuarioController;
-
-
     private static final Logger logger = LoggerFactory.getLogger(PersonaController.class);
-
-    /*@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String listUsers(ModelMap model) {
-        model.addAttribute("user", new Persona());
-        model.addAttribute("users", personaService.findAll() /* session.createCriteria(com.springapp.mvc.User.class).list());
-        return "users";
-    }*/
-
-    /*@RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addUser(@ModelAttribute("user") Persona user, BindingResult result) {
-
-        personaService.create(user);
-
-        return "redirect:/";
-    }*/
 
     @GetMapping
     public ResponseEntity listUsers(String nombre, String dni) throws JSONException {
@@ -49,8 +29,7 @@ public class PersonaController {
                     return ResponseEntity.status(HttpStatus.NO_CONTENT).body("no hay usuarios con el DNI:" + dni);
                 } else {
                     List<Persona> listaU = personaService.findAllByDni(dni);
-                    String json = jsonUsuarioController.getList(listaU);
-                    return ResponseEntity.status(HttpStatus.OK).body(json);
+                    return ResponseEntity.status(HttpStatus.OK).body(listaU);
                 }
             }
 
@@ -59,8 +38,7 @@ public class PersonaController {
                     return ResponseEntity.status(HttpStatus.NO_CONTENT).body("no hay usuarios con el Nombre o Apellido:" + nombre);
                 } else {
                     List<Persona> listaU = personaService.findAllByNombreAndApellido(nombre);
-                    String json = jsonUsuarioController.getList(listaU);
-                    return ResponseEntity.status(HttpStatus.OK).body(json);
+                    return ResponseEntity.status(HttpStatus.OK).body(listaU);
                 }
             }
 
@@ -68,8 +46,7 @@ public class PersonaController {
                 return ResponseEntity.status(HttpStatus.NO_CONTENT).body("no hay datos");
             } else {
                 List<Persona> listaU = personaService.findAll();
-                String json = jsonUsuarioController.getList(listaU);
-                return ResponseEntity.status(HttpStatus.OK).body(json);
+                return ResponseEntity.status(HttpStatus.OK).body(listaU);
             }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
@@ -83,8 +60,7 @@ public class PersonaController {
                 return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Persona no encontrado");
             } else {
                 Persona persona = personaService.findById(idUser);
-                String json = jsonUsuarioController.getObj(persona);
-                return ResponseEntity.status(HttpStatus.OK).body(json);
+                return ResponseEntity.status(HttpStatus.OK).body(persona);
             }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
